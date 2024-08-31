37cbf19427471d1c1705efd3054fdba4841644fc
Luigi <luigimete54@gmail.com>
2024-08-31 12:15:28 -0300

    getData funciona

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hola SPA')
+import getData from "./src/utils/getData.js"
+
+console.log('Hola')
+
+const character = await getData(1);
+console.log('Nombre: ', character.name)
+console.log('Especie: ', character.species)
+console.log('Estado: ', character.status)
+console.log('Origen: ', character.origin)
+console.log('Genero: ', character.gender)
+console.log('Ubicacion: ', character.location)
+console.log('Link Imagen: ', character.image)
+
+
+const index = async () => {
+    const view = `
+    <div class="Characters">
+        <article class="Character-item">
+            <a href="#/1/">
+                <img src="image" alt="name">
+                <h2>Name</h2>
+            </a>
+        </article>  
+    </div>
+    `
+    return view
+}
+
+export default index
